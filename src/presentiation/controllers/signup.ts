aa97a9c5aed0309a204a8c1881393f3bb5003eb6
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-10-04 09:39:52 -0300

    feat: ensure SignUpController returns an error id no email is provided

--- src/presentiation/controllers/signup.ts
+++ src/presentiation/controllers/signup.ts
@@ -6,6 +6,12 @@ export class SignUpController {
         body: new Error('Missing param: name'),
       };
     }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email'),
+      };
+    }
     return {
       statusCode: 200,
       body: {},
