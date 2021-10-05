13a76801b1b58f9ce235f50aca5d9f1f89242504
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-10-05 17:37:51 -0300

    feat: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,17 @@
+import validator from 'validator';
 import { EmailValidatorAdapter } from './email-validator-adapter';
 
 describe('EmailValidator Adapter', () => {
   it('Should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter();
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false);
     const isValid = sut.isValid('invalid_email@mail.com');
     expect(isValid).toBe(false);
+  });
+
+  it('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter();
+    const isValid = sut.isValid('any_email@mail.com');
+    expect(isValid).toBe(true);
   });
 });
