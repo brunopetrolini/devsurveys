15e6e682efd669dacd9357fdc88b92c940060a8d
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-10-06 17:53:21 -0300

    feat: ensure cors is enable

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,15 @@
+import supertest from 'supertest';
+import { app } from '../config/app';
+
+describe('CORS Middleware', () => {
+  it('Should enable CORS', async () => {
+    app.get('/test-cors', (request, response) => {
+      response.send();
+    });
+    await supertest(app)
+      .get('/test-cors')
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*');
+  });
+});
