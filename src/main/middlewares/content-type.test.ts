eb1c0b889c04310fee9f3b931a76f06af71df818
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-10-06 18:02:10 -0300

    test: ensure return xml content type when forced

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -3,11 +3,21 @@ import { app } from '../config/app';
 
 describe('Content-Type Middleware', () => {
   it('Should return default content type as json', async () => {
-    app.get('/test-content-type', (request, response) => {
+    app.get('/test-content-type-json', (request, response) => {
       response.send();
     });
     await supertest(app)
-      .get('/test-content-type')
+      .get('/test-content-type-json')
       .expect('content-type', /json/);
+  });
+
+  it('Should return xml content type when forced', async () => {
+    app.get('/test-content-type-xml', (request, response) => {
+      response.type('xml');
+      response.send();
+    });
+    await supertest(app)
+      .get('/test-content-type-xml')
+      .expect('content-type', /xml/);
   });
 });
