c0f4fc4d78cea2671a972881683e6d128449045e
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-10-14 18:21:23 -0300

    refactor: move sut creation to a helper factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -20,19 +20,22 @@ const makeController = (): Controller => {
   return new ControllerStub();
 };
 
+type SutTypes = {
+  controllerStub: Controller,
+  sut: LogControllerDecorator,
+}
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController();
+  const sut = new LogControllerDecorator(controllerStub);
+  return { controllerStub, sut };
+};
+
 describe('LogControllerDecorator', () => {
   it('Should call controller handle', async () => {
-    const controllerStub = makeController();
+    const { sut, controllerStub } = makeSut();
     const handleSpy = jest.spyOn(controllerStub, 'handle');
-    const sut = new LogControllerDecorator(controllerStub);
-    await sut.handle(makeFakeRequest());
-    expect(handleSpy).toHaveBeenCalledWith({
-      body: {
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password',
-      },
-    });
+    const httpRequest = makeFakeRequest();
+    await sut.handle(httpRequest);
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest);
   });
 });
