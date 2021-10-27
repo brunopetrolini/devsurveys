eeb02340e1fc8d31e54b76d19f5e079542a607ea
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-10-27 16:29:14 -0300

    feat: ensure LoginController returns 400 if no password is provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,10 +1,16 @@
 import { MissingParamError } from '../../errors';
-import { badRequest } from '../../helpers/http-helper';
+import { badRequest, ok } from '../../helpers/http-helper';
 import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 
 export class LoginController implements Controller {
   // eslint-disable-next-line no-unused-vars
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    return badRequest(new MissingParamError('email'));
+    const requiredFields = ['email', 'password'];
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
+    }
+    return ok({});
   }
 }
