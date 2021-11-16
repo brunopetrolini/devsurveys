b057bf3e96a20f0d46415d505e85f95c1a336ba4
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-11-16 18:12:59 -0300

    feat: ensure AddSurveyController calls AddSurvey with correct values

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,6 +1,10 @@
+/* eslint-disable max-classes-per-file */
 /* eslint-disable no-unused-vars */
+import { badRequest } from '../../../helpers/http/http-helper';
 import { AddSurveyController } from './add-survey-controller';
-import { badRequest, HttpRequest, Validation } from './add-survey-controller-protocols';
+import {
+  AddSurvey, AddSurveyModel, HttpRequest, Validation,
+} from './add-survey-controller-protocols';
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -21,14 +25,25 @@ const makeValidation = (): Validation => {
   return new ValidationStub();
 };
 
+const makeAddSurvey = (): AddSurvey => {
+  class AddSurveyStub implements AddSurvey {
+    async add(data: AddSurveyModel): Promise<void> {
+      Promise.resolve();
+    }
+  }
+  return new AddSurveyStub();
+};
+
 type SutTypes = {
   sut: AddSurveyController;
   validationStub: Validation;
+  addSurveyStub: AddSurvey;
 }
 const makeSut = (): SutTypes => {
   const validationStub = makeValidation();
-  const sut = new AddSurveyController(validationStub);
-  return { sut, validationStub };
+  const addSurveyStub = makeAddSurvey();
+  const sut = new AddSurveyController(validationStub, addSurveyStub);
+  return { sut, validationStub, addSurveyStub };
 };
 
 describe('AddSurvey Controller', () => {
@@ -45,5 +60,13 @@ describe('AddSurvey Controller', () => {
     jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error());
     const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse).toEqual(badRequest(new Error()));
+  });
+
+  it('Should call AddSurvey with correct values', async () => {
+    const { sut, addSurveyStub } = makeSut();
+    const addSpy = jest.spyOn(addSurveyStub, 'add');
+    const httpRequest = makeFakeRequest();
+    await sut.handle(httpRequest);
+    expect(addSpy).toHaveBeenCalledWith(httpRequest.body);
   });
 });
--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,12 +1,15 @@
+import { badRequest, ok } from '../../../helpers/http/http-helper';
 import {
-  Controller, Validation, HttpRequest, HttpResponse, ok, badRequest,
+  Controller, Validation, HttpRequest, HttpResponse, AddSurvey,
 } from './add-survey-controller-protocols';
 
 export class AddSurveyController implements Controller {
   private readonly validations: Validation;
+  private readonly addSurvey: AddSurvey;
 
-  constructor(validations: Validation) {
+  constructor(validations: Validation, addSurvey: AddSurvey) {
     this.validations = validations;
+    this.addSurvey = addSurvey;
   }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -14,6 +17,7 @@ export class AddSurveyController implements Controller {
     if (error) {
       return badRequest(error);
     }
+    await this.addSurvey.add(httpRequest.body);
     return ok({});
   }
 }
